66028f11aaf6ed26712b1bfedbdc6dc5fb07092e
baikin-man <2401351@s.asojuku.ac.jp>
2025-10-23 09:00:35 +0900

    Update G2.php

--- G2.php
+++ G2.php
@@ -14,6 +14,41 @@
     <div id="background">
         <div class="container">
 
+            <div class='banner'>
+
+            </div>
+
+            <div class='content1'>
+                <div class='mans'></div>
+                <div class='ladies'></div>
+                <div class='kids'></div>
+                <div class='sale'></div>
+            </div>
+
+            <div class='content2'>
+                <div class='search'>
+                    <h3>探す</h3>
+                    <ul>
+                        <li><a href=''></a></li>
+                        <li><a href=''></a></li>
+                        <li><a href=''></a></li>
+                    </ul>
+                </div>
+                <div class='search'>
+                    <h3>カテゴリー</h3>
+                    <ul>
+                        <li><a href=''></a></li>
+                        <li><a href=''></a></li>
+                        <li><a href=''></a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class='content3'>
+                <div class='pop'>
+                    
+                </div>
+            </div>
         </div>
     </div>
 
