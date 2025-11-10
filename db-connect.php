03878d50cbb3f863560ae4e2295379c8e9e14f06
baikin-man <2401351@s.asojuku.ac.jp>
2025-11-10 09:51:14 +0900

    g3, connect

---
+++ db-connect.php
@@ -0,0 +1,7 @@
+<?php
+const SERVER = 'mysql327.phy.lolipop.lan';
+const DBNAME = 'LAA1607961-zeze';
+const USER = 'LAA1607961';
+const PASS = 'zeze2025';
+$connect = 'mysql:host=', SERVER , ';dbname=' , DBNAME ,';charset=utf8';
+?>
