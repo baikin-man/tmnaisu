f9c23d4fc20e41d3eb82f1dad64702974bc815b4
you000-000 <yuki.matu.fuku@gmail.com>
2025-11-12 12:23:04 +0900

    Update header1.php

--- header1.php
+++ header1.php
@@ -1,11 +1,17 @@
+<!DOCTYPE html>
 <html lang="ja">
 <head>
-    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header1.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+    <title>Document</title>
+</head>
+<body>
 
-        <!-- header.php -->
+    <!-- header.php -->
 <header class="header">
-    <h1 id="center">ZeZe</h1>
+    <h1 id="center"><a href="G2.php">ZeZe</a></h1>
 
 
 <i class="fas fa-shopping-cart" id = "cart-icon"></i>
@@ -24,15 +30,24 @@
   
   <nav class="menu">
     <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Service</a></li>
-      <li><a href="#">Contact</a></li>
+      <h2>人気・新着の商品</h2>
+      <li><a href="#">ランキング</a></li>
+      <li><a href="#">新着商品</a></li>
+      <p>────────────</p>
+      <h2>設定とヘルプ</h2>
+      <li><a href="#">アカウントサービス</a></li>
+      <li><a href="#">住所を変更</a></li>
+      <li><a href="#">日本語</a></li>
+      <li><a href="#">ログアウト</a></li>
+      <p>────────────</p>
+      <li><a href="#">お問い合わせ</a></li>
+
     </ul>
   </nav>
 </div>
 </div>
 
 
-</div>
     
+</body>
+</html>
