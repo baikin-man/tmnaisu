5eacbc560fb8245f3f1bb102c29141408d51d700
baikin-man <2401351@s.asojuku.ac.jp>
2025-11-13 09:36:24 +0900

    a

--- G4.php
+++ G4.php
@@ -6,11 +6,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='stylesheet' href='./css/G4.css'>
-    <link rel='stylesheet' href='./css/header.css'>
+    <link rel='stylesheet' href='./css/header2.css'>
     <title>ZeZe</title>
 </head>
 <body>
-    <?php require 'header1.php';?>
+    <?php require 'header2.php';?>
 
     <div id="background">
         <div class="container">
